38a01271b324b2864d571ce9017cfa4ffa49d1f6
adammclean14 <adamcharlesmclean@hotmail.com>
2020-11-29 20:48:22 +0000

    TransferMoney refactor, execute method in WithrdrawMoney added and Account properties and methods changed

--- Moneybox.App/Domain/Account.cs
+++ Moneybox.App/Domain/Account.cs
@@ -4,16 +4,38 @@ namespace Moneybox.App
 {
     public class Account
     {
+        public const decimal WithdrawLimit = 0m;
         public const decimal PayInLimit = 4000m;
-
+        public const decimal LowFundsLimit = 500m;
+        public const decimal PayInNotificationLimit = 500m;
         public Guid Id { get; set; }
-
         public User User { get; set; }
-
         public decimal Balance { get; set; }
-
         public decimal Withdrawn { get; set; }
-
         public decimal PaidIn { get; set; }
+        public bool IsWithdrawPossible(decimal withdrawAmount) => Balance - withdrawAmount >= WithdrawLimit;
+        public bool IsPayInPossible(decimal payInAmount) => PaidIn + payInAmount <= PayInLimit;
+        public bool IsLowFunds => Balance < LowFundsLimit;
+        public bool IsNearlyAtPayLimit => PayInLimit - PaidIn < PayInNotificationLimit;
+        public void Withdraw(decimal withdrawAmount)
+        {
+            if (!IsWithdrawPossible(withdrawAmount))
+            {
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+            }
+
+            Balance -= withdrawAmount;
+            Withdrawn -= withdrawAmount;
+        }
+        public void PayIn(decimal payInAmount)
+        {
+            if (!IsPayInPossible(payInAmount))
+            {
+                throw new InvalidOperationException("Account pay in limit reached");
+            }
+
+            Balance += payInAmount;
+            PaidIn += payInAmount;
+        }
     }
 }
--- Moneybox.App/Features/WithdrawMoney.cs
+++ Moneybox.App/Features/WithdrawMoney.cs
@@ -1,23 +1,34 @@
-﻿using Moneybox.App.DataAccess;
-using Moneybox.App.Domain.Services;
+﻿using Moneybox.App.Domain.Services;
+using Moneybox.App.DataAccess;
 using System;
 
 namespace Moneybox.App.Features
 {
     public class WithdrawMoney
     {
-        private IAccountRepository accountRepository;
-        private INotificationService notificationService;
+        private IAccountRepository _accountRepository;
+        private INotificationService _notificationService;
 
         public WithdrawMoney(IAccountRepository accountRepository, INotificationService notificationService)
         {
-            this.accountRepository = accountRepository;
-            this.notificationService = notificationService;
+            _accountRepository = accountRepository;
+            _notificationService = notificationService;
         }
 
         public void Execute(Guid fromAccountId, decimal amount)
         {
-            // TODO:
+            var fromAccount = _accountRepository.GetAccountById(fromAccountId);
+            fromAccount.Withdraw(amount);
+            _accountRepository.Update(fromAccount);
+            NotifyEmailIfFundsLow(fromAccount);
+        }
+
+        private void NotifyEmailIfFundsLow(Account fromAccount)
+        {
+            if (fromAccount.IsLowFunds)
+            {
+                _notificationService.NotifyFundsLow(fromAccount.User.Email);
+            }
         }
     }
 }
